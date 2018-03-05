0f20bab674f0597c7dafdd66e365ebc442df7287
Sukhjinder <nahal.snahal@gmail.com>
2018-03-05 15:21:06 -0500

    Implemented Parcelable

--- app/src/main/java/com/nahal/sukhjinder/the_real_mvp/City.java
+++ app/src/main/java/com/nahal/sukhjinder/the_real_mvp/City.java
@@ -1,10 +1,13 @@
 package com.nahal.sukhjinder.the_real_mvp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Sukhjinder on 3/4/18.
  */
 
-public class City {
+public class City implements Parcelable {
 
     private String cityName;
     private String funFact;
@@ -47,4 +50,37 @@ public class City {
     public void setCityImage(int cityImage) {
         this.cityImage = cityImage;
     }
+
+    protected City(Parcel in) {
+        cityName = in.readString();
+        funFact = in.readString();
+        sites = in.readString();
+        cityImage = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(cityName);
+        dest.writeString(funFact);
+        dest.writeString(sites);
+        dest.writeInt(cityImage);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<City> CREATOR = new Parcelable.Creator<City>() {
+        @Override
+        public City createFromParcel(Parcel in) {
+            return new City(in);
+        }
+
+        @Override
+        public City[] newArray(int size) {
+            return new City[size];
+        }
+    };
 }

afbeef56c635991f914852329e87c3e1cde9c07d
Sukhjinder <nahal.snahal@gmail.com>
2018-03-06 00:25:33 -0500

    made a few minor changes to weather layout

--- app/src/main/java/com/nahal/sukhjinder/the_real_mvp/CityListPresenter.java
+++ app/src/main/java/com/nahal/sukhjinder/the_real_mvp/CityListPresenter.java
@@ -16,7 +16,7 @@ public class CityListPresenter {
 
     String newYorkCityDescription = "Situated on one of the world's largest natural harbors, New York City consists of five boroughs, each of which is a separate county of New York State. The five boroughs – Brooklyn, Queens, Manhattan, The Bronx, and Staten Island – were consolidated into a single city in 1898.";
     Site newYorkCitySite = new Site("Statue Of Liberty", R.drawable.statue_of_liberty, "The Statue of Liberty is a world-famous symbol of freedom, given in the 1880s by France to the United States in celebration of friendship. Nearby Ellis Island was the first stop for millions of immigrants to the U.S. in the late 19th and early 20th centuries.");
-    Weather newYorkCityWeather = new Weather("40 F", "20%", "Clear Sky", "4 Mph", R.drawable.art_clear);
+    Weather weather = new Weather("40 F", "20%", "Clear Sky", "4 Mph", R.drawable.art_clear);
     Restaurant newYorkCityRestaurant = new Restaurant("Grimaldi's", "Grimaldi's Pizzeria is one of the most popular pizzerias in New York City. In recent years, Zagat Survey rated Grimaldi's No. 1 Pizzeria in New York.", R.drawable.grimaldis);
 
 
@@ -24,18 +24,18 @@ public class CityListPresenter {
 
 
     public void loadCities() {
-        City newYorkCity = new City("New York City", R.drawable.new_york_city, newYorkCityDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
+        City newYorkCity = new City("New York City", R.drawable.new_york_city, newYorkCityDescription, newYorkCitySite, weather, newYorkCityRestaurant);
 
 //      Reused details from new york city to simplify
-        City boston = new City("Boston", R.drawable.boston, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City chicago = new City("Chicago", R.drawable.chicago, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City miami = new City("Miami", R.drawable.miami, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City dallas = new City("Dallas", R.drawable.dallas, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City honolulu = new City("Honolulu", R.drawable.honolulu, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City lasVegas = new City("Las Vegas", R.drawable.las_vegas, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City losAngeles = new City("Los Angeles", R.drawable.los_angeles, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City seattle = new City("Seattle", R.drawable.seattle, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
-        City washington_dc = new City("Washington D.C.", R.drawable.washington_dc, defaultDescription, newYorkCitySite, newYorkCityWeather, newYorkCityRestaurant);
+        City boston = new City("Boston", R.drawable.boston, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City chicago = new City("Chicago", R.drawable.chicago, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City miami = new City("Miami", R.drawable.miami, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City dallas = new City("Dallas", R.drawable.dallas, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City honolulu = new City("Honolulu", R.drawable.honolulu, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City lasVegas = new City("Las Vegas", R.drawable.las_vegas, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City losAngeles = new City("Los Angeles", R.drawable.los_angeles, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City seattle = new City("Seattle", R.drawable.seattle, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
+        City washington_dc = new City("Washington D.C.", R.drawable.washington_dc, defaultDescription, newYorkCitySite, weather, newYorkCityRestaurant);
 
         cities.add(newYorkCity);
         cities.add(boston);

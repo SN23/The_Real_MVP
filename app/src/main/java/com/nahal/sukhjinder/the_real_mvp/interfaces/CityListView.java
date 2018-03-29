4970f203af76869f7823a6fd822d5bfacce0d2a3
Sukhjinder <nahal.snahal@gmail.com>
2018-03-29 14:49:38 -0400

    created interfaces package

---
+++ app/src/main/java/com/nahal/sukhjinder/the_real_mvp/interfaces/CityListView.java
@@ -0,0 +1,14 @@
+package com.nahal.sukhjinder.the_real_mvp.interfaces;
+
+import com.nahal.sukhjinder.the_real_mvp.model.City;
+
+import java.util.List;
+
+/**
+ * Created by Sukhjinder on 3/28/18.
+ */
+
+public interface CityListView {
+
+    List<City> loadCities();
+}
